a9a89ad91a8e3df09c1dc3562a8ade0c3c2a1c20
josedanielchg <j.daniel.chg@gmail.com>
2026-05-22 11:45:28 +0200

    Added ROS simulation workspace packages

---
+++ simulation/ros2_ws/src/footbot_perception/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'footbot_perception'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='josedanielchg',
+    maintainer_email='j.daniel.chg@gmail.com',
+    description='Perception package for the Footbot simulation.',
+    license='MIT',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
